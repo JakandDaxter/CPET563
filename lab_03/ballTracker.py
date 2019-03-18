9425f6d7792d52a57f20a9ec0114092fca30a7ef
Adib Yahaya <adib.yhy@gmail.com>
2019-03-17 21:47:36 -0400

    Add signal handler to sliders

--- lab_03/ballTracker.py
+++ lab_03/ballTracker.py
@@ -62,26 +62,44 @@ class BallTracker(QWidget):
     self.rMinLabel = QLabel(self)
     self.rMinLabel.setText("Red min")
     self.rMinSlider = QSlider(Qt.Horizontal)
+    self.rMinSlider.setMinimum(0)
+    self.rMinSlider.setMaximum(255)
+    self.rMinSlider.valueChanged.connect(self.sliderValueChanged_redMin)
 
     self.rMaxLabel = QLabel(self)
     self.rMaxLabel.setText("Red max")
     self.rMaxSlider = QSlider(Qt.Horizontal)
+    self.rMaxSlider.setMinimum(0)
+    self.rMaxSlider.setMaximum(255)
+    self.rMaxSlider.valueChanged.connect(self.sliderValueChanged_redMax)
 
     self.gMinLabel = QLabel(self)
     self.gMinLabel.setText("Green min")
     self.gMinSlider = QSlider(Qt.Horizontal)
+    self.gMinSlider.setMinimum(0)
+    self.gMinSlider.setMaximum(255)
+    self.gMinSlider.valueChanged.connect(self.sliderValueChanged_greenMin)
 
     self.gMaxLabel = QLabel(self)
     self.gMaxLabel.setText("Green max")
     self.gMaxSlider = QSlider(Qt.Horizontal)
+    self.gMaxSlider.setMinimum(0)
+    self.gMaxSlider.setMaximum(255)
+    self.gMaxSlider.valueChanged.connect(self.sliderValueChanged_greenMax)
 
     self.bMinLabel = QLabel(self)
     self.bMinLabel.setText("Blue min")
     self.bMinSlider = QSlider(Qt.Horizontal)
+    self.bMinSlider.setMinimum(0)
+    self.bMinSlider.setMaximum(255)
+    self.bMinSlider.valueChanged.connect(self.sliderValueChanged_blueMin)
 
     self.bMaxLabel = QLabel(self)
     self.bMaxLabel.setText("Blue max")
     self.bMaxSlider = QSlider(Qt.Horizontal)
+    self.bMaxSlider.setMinimum(0)
+    self.bMaxSlider.setMaximum(255)
+    self.bMaxSlider.valueChanged.connect(self.sliderValueChanged_blueMax)
 
     layout_selectBall.addWidget(self.ballSelectLabel)
     layout_selectBall.addWidget(self.blueBallBtn)
@@ -154,6 +172,60 @@ class BallTracker(QWidget):
     self.greenBallBtn.setChecked(False)
     self.ballColorGroup.setExclusive(True)
 
+  def sliderValueChanged_redMin(self):
+    value = self.rMinSlider.value()
+    self.rMin = value
+    self.uncheckRadioButton()
+    try:
+      self.doTracker(self.small_img)
+    except:
+      pass
+
+  def sliderValueChanged_redMax(self):
+    value = self.rMaxSlider.value()
+    self.rMax = value
+    self.uncheckRadioButton()
+    try:
+      self.doTracker(self.small_img)
+    except:
+      pass
+
+  def sliderValueChanged_greenMin(self):
+    value = self.gMinSlider.value()
+    self.gMin = value
+    self.uncheckRadioButton()
+    try:
+      self.doTracker(self.small_img)
+    except:
+      pass
+
+  def sliderValueChanged_greenMax(self):
+    value = self.gMaxSlider.value()
+    self.gMax = value
+    self.uncheckRadioButton()
+    try:
+      self.doTracker(self.small_img)
+    except:
+      pass
+
+  def sliderValueChanged_blueMin(self):
+    value = self.bMinSlider.value()
+    self.bMin = value
+    self.uncheckRadioButton()
+    try:
+      self.doTracker(self.small_img)
+    except:
+      pass
+
+  def sliderValueChanged_blueMax(self):
+    value = self.bMaxSlider.value()
+    self.bMax = value
+    self.uncheckRadioButton()
+    try:
+      self.doTracker(self.small_img)
+    except:
+      pass
+
   def displayCentroidValue(self, which, val):
     if val != "" and val != 0:
       if which == "X":
@@ -168,42 +240,46 @@ class BallTracker(QWidget):
       self.YcentroidValueLabel.setText("NA")
 
   def doTracker(self, small_img):
-    x = 0
-    y = 0
-    # generate threshold array
-    lower = np.array([self.bMin,self.gMin,self.rMin])
-    upper = np.array([self.bMax,self.gMax,self.rMax])
+    if not small_img.all():
+      # No image loaded
+      pass
+    else:
+      x = 0
+      y = 0
+      # generate threshold array
+      lower = np.array([self.bMin,self.gMin,self.rMin])
+      upper = np.array([self.bMax,self.gMax,self.rMax])
 
-    mask = cv2.inRange(small_img, lower, upper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+      mask = cv2.inRange(small_img, lower, upper)
+      mask = cv2.erode(mask, None, iterations=2)
+      mask = cv2.dilate(mask, None, iterations=2)
 
-    maskedImage = cv2.bitwise_and(small_img,small_img,mask=mask)
+      maskedImage = cv2.bitwise_and(small_img,small_img,mask=mask)
 
-    clone_img = copy.copy(small_img)
+      clone_img = copy.copy(small_img)
 
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-      # find the largest contour in the mask, then use
-      # it to compute the minimum enclosing circle and
-      # centroid
-      c = max(cnts, key=cv2.contourArea)
-      ((x, y), radius) = cv2.minEnclosingCircle(c)
-      M = cv2.moments(c)
-      center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+      cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+      center = None
+      # only proceed if at least one contour was found
+      if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-      # only proceed if the radius meets a minimum size
-      if radius > 10:
-        # draw the circle and centroid on the frame,
-        # then update the list of tracked points
-        cv2.circle(clone_img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-        cv2.circle(clone_img, center, 5, (0, 0, 255), -1)
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+          # draw the circle and centroid on the frame,
+          # then update the list of tracked points
+          cv2.circle(clone_img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+          cv2.circle(clone_img, center, 5, (0, 0, 255), -1)
 
-      cv2.imshow('Result',clone_img)
-    self.displayCentroidValue("X", x)
-    self.displayCentroidValue("Y", y)
+        cv2.imshow('Result',clone_img)
+      self.displayCentroidValue("X", x)
+      self.displayCentroidValue("Y", y)
 
 ###############################################################################
 # Load an image file
@@ -214,9 +290,9 @@ class BallTracker(QWidget):
       pass
     else:
       img = cv2.imread(str(fileName))
-      small_img = cv2.resize(img,(640,480))
-      # cv2.imshow(str(fileName),small_img)
-      self.doTracker(small_img)
+      self.small_img = cv2.resize(img,(640,480))
+      # cv2.imshow(str(fileName),self.small_img)
+      self.doTracker(self.small_img)
 
 ###############################################################################
 # save and load ball detection parameters files
